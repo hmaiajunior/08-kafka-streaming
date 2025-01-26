185d07c4ee645e3ff1ae03dfb8749c730d7c0359
hmaiajunior <juniorce04@gmail.com>
2025-01-26 07:36:40 -0300

    finalizando producer - sensor de refrigerador

--- kafka-producer/kafka-producer.py
+++ kafka-producer/kafka-producer.py
@@ -15,3 +15,24 @@ conf = {
     #'sasl.password': os.getenv('SASL_PASSWORD'),
     'client.id': os.getenv('CLIENT_ID')
 }
+
+producer = Producer(**conf)
+
+# Função de callback para entrega de mensagens
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
+
+topic = os.getenv('TOPIC')
+
+for i in range(10):
+    temperature = random.uniform(-5, 5)  # Temperatura aleatória entre -5 e 5 graus Celsius
+    key = f"{(i % 3) + 1}"  # Usar diferentes chaves para distribuir entre partições
+    producer.produce(topic, key=key, value=f"{temperature:.2f}", callback=delivery_report)
+    producer.poll(0)
+    time.sleep(1) # Simula leitura de temperatura a cada segundo
+
+# Espera até todas as mensagens serem entregues
+producer.flush()
