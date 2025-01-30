fc82ae18618a8f58491b8e9e4389603f072c9a81
hmaiajunior <juniorce04@gmail.com>
2025-01-29 21:14:13 -0300

    finalizando consumer

--- kafka-consumer/kafka-consumer.py
+++ kafka-consumer/kafka-consumer.py
@@ -23,3 +23,36 @@ consumer = Consumer(**conf)
 # Inscrição no tópico
 topic = os.getenv('TOPIC')
 consumer.subscribe([topic])
+
+# Função para processar mensagens
+def consume_messages():
+    # Nome do arquivo CSV
+    script_dir = os.path.dirname(os.path.abspath(__file__))  # Diretório do script atual
+    csv_file = os.path.join(script_dir, 'messages.csv')  # Caminho completo para o arquivo CSV
+    
+    # Abrir o arquivo CSV em modo de escrita
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Key', 'Value'])  # Escrever cabeçalho no CSV
+        
+        try:
+            while True:
+                msg = consumer.poll(1.0)  # Espera até 1 segundo por uma nova mensagem
+                # Exibe a mensagem recebida
+                if msg is None:  # Se nenhuma mensagem foi recebida
+                    continue
+                if msg.error():  # Se houve um erro ao receber a mensagem
+                    continue
+                key = msg.key().decode('utf-8')
+                value = msg.value().decode('utf-8')
+                print(f"Received message: Key: {key}, Value: {value}")
+                # Escreve a mensagem no CSV
+                writer.writerow([key, value])
+        except KeyboardInterrupt:
+            pass
+        finally:
+            print("Closing consumer.")
+            consumer.close()
+
+if __name__ == "__main__":
+    consume_messages()
